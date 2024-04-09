d060e3f1f2dfad219848642f0c838db184312336
ULTRACODE <macbook@MACBOOKs-MacBook-Pro.local>
2024-04-09 01:42:08 +0100

    Added the search functionality

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import PowerIcon from "./icons/PowerIcon";
 import data from "../constant/data";
 import { auth } from "../firebase";
 import { signOut } from "firebase/auth";
 import { toast } from "react-toastify";
+import { AuthContext } from "../context/AuthContext";
 
 const NavBar = () => {
+  const { currentUser } = useContext(AuthContext);
   const { userProfile } = data;
   const handleSignOut = () => {
     signOut(auth);
@@ -16,8 +18,12 @@ const NavBar = () => {
     <div className="w-full  bg-gray-900 px-2 py-3 flex items-center rounded-tl-lg  justify-between border-b-2 border-forground">
       <span className="text-xl font-extrabold text-primary">UltraChat</span>
       <div className="flex items-center justify-between gap-2 border-l-2 border-gray-700 pl-4">
-        <img src={userProfile} className="w-8 h-8 rounded-full" alt="" />
-        <span>Akachukwu</span>
+        <img
+          src={currentUser.photoURL}
+          className="w-8 h-8 rounded-full"
+          alt=""
+        />
+        <span>{currentUser.displayName}</span>
         <button onClick={handleSignOut} className="text-xs">
           <PowerIcon />
         </button>
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,12 +1,54 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useState } from "react";
+import { db } from "../firebase";
+
 const SearchBox = () => {
+  const [username, setUsername] = useState("")
+  const [user, setUser] = useState(null)
+  const [err, setErr] = useState(false)
+
+  const handleSearch = async ()=>{
+    const q = query(collection(db, "users"), where("fullname", "==", username));
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        setUser(doc.data())
+        console.log(doc.data())
+        
+      });
+    } catch (error) {
+      setErr(true)
+      console.log(error)
+    }
+  }
+  
+  const handleKey = (e)=>{
+    e.code === "Enter" && handleSearch()
+  }
   return (
+    <>
     <div className=" w-full border-b-2 border-b-forground">
       <input
         type="text"
         placeholder="Find users"
+        value={username}
+        onKeyDown={handleKey}
+        onChange={(e)=>setUsername(e.target.value)}
         className="w-full bg-transparent py-2 px-4 focus:outline-none"
       />
     </div>
+    {user && (
+      <div className="flex items-center cursor-pointer justify-start gap-2 border-b-2 border-gray-700 px-4 py-2 hover:bg-gray-900">
+      <img src={user.photoUrl} className="w-8 h-8 rounded-full" alt="" />
+      <div className="flex flex-col">
+        <span className="font-bold">{user.fullname}</span>
+        <small className="text-gray-500">Last msg here...</small>
+      </div>
+    </div>
+    )}
+    </>
   );
 };
 
